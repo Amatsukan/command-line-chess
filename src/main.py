b2c7b7fbfaa799846e82355e58afbe86dc097b20
Luiz.Ferreira <luiz.ferreira@khomp.com>
2017-06-22 20:21:48 -0300

    Evolution, next step: P2P

--- src/main.py
+++ src/main.py
@@ -7,10 +7,9 @@ import random
 WHITE = True
 BLACK = False
 
-
 def askForPlayerSide():
     playerChoiceInput = input(
-        "What side would you like to play as [wB]? ").lower()
+        "What side would you like to play as [w/B]? ").lower()
     if 'w' in playerChoiceInput:
         print("You will play as white")
         return WHITE
@@ -18,6 +17,22 @@ def askForPlayerSide():
         print("You will play as black")
         return BLACK
 
+AIxAI = 0
+PVP   = 1
+PVE   = 2
+
+def GameMode():
+    print("GameModes:\n\tAi versus ai : 0\n\tPlayer versus player : 1\n\tAi versus ai : 2")
+    playerChoiceInput = input("Game mode to be used[0/1/2]?")
+    try:
+        mode = int(playerChoiceInput)
+        if not (mode < 0 or mode > 2):
+            return mode
+        else:
+            raise ValueError('Only value between 0 and 2')
+    except ValueError as e:
+            print("Incorrect answer:{}".format(str(e)))
+            return GameMode()
 
 def askForDepthOfAI():
     depthInput = 2
@@ -70,7 +85,7 @@ def undoLastTwoMoves(board):
         board.undoLastMove()
 
 
-def startGame(board, playerSide, ai):
+def startGameAi(board, playerSide, ai):
     parser = InputParser(board, playerSide)
     while True:
         print(board)
@@ -120,13 +135,107 @@ def startGame(board, playerSide, ai):
             move.notation = parser.notationForMove(move)
             makeMove(move, board)
 
+
+def startGameAIxAI(board, aiWhite, aiBlack):
+    parser = InputParser(board, playerSide)
+    while True:
+        print(board)
+        print()
+        if board.isCheckmate():
+            if board.currentSide == WHITE:
+                print("Checkmate, White AI wins!")
+            else:
+                print("Checkmate! Black AI wins!")
+            return
+
+        if board.isStalemate():
+                print("Stalemate")
+
+        if board.currentSide == WHITE:
+            print("White AI thinking...")
+            move = aiWhite.getBestMove()
+            move.notation = parser.notationForMove(move)
+            makeMove(move, board)
+
+        else:
+            print("Black AI thinking...")
+            move = aiBlack.getBestMove()
+            move.notation = parser.notationForMove(move)
+            makeMove(move, board)
+
+def startGameX1(board):
+    white_turn = True;
+    parser = InputParser(board, WHITE)
+    while True:
+        print(board)
+        print()
+        if board.isCheckmate():
+            if board.currentSide == WHITE:
+                print("Checkmate, WHITE win!")
+            else:
+                print("Checkmate! BLACK win!")
+            return
+
+        if board.isStalemate():
+                print("Stalemate")
+
+        move = None
+        command = None
+        changeColor = False
+        if board.currentSide == WHITE:
+            command = input("WHITE:"
+                            " Type '?' for options. ? ").lower()
+        else:
+            command = input("BLACK:"
+                            " Type '?' for options. ? ").lower()
+            
+        # printPointAdvantage(board)
+
+        print("command: {}".format(command))
+       
+        if command == 'u':
+            undoLastTwoMoves(board)
+            continue
+        elif command == '?':
+            printCommandOptions()
+            continue
+        elif command == 'l':
+            printAllLegalMoves(board, parser)
+            continue
+        elif command == 'r':
+            move = getRandomMove(board, parser)
+        elif command == 'quit':
+            return
+        else:
+            move = parser.moveForShortNotation(command)
+
+        if move:
+            changeColor = True
+            print("MOVE")
+            board.makeMove(move)
+        else:
+            print("Couldn't parse input, enter a valid command or move.")
+
 board = Board()
-playerSide = askForPlayerSide()
+playerSide = WHITE
+GM = GameMode()
 print()
-aiDepth = askForDepthOfAI()
-opponentAI = AI(board, not playerSide, aiDepth)
 
 try:
-    startGame(board, playerSide, opponentAI)
+    if(GM != PVP)
+        aiDepth = askForDepthOfAI()
+    
+
+    if GM == PVP:
+        startGameX1(board)
+    elif GM == AIxAI:
+        WHITEai = AI(board, WHITE, aiDepth)
+        BLACKai = AI(board, BLACK, aiDepth)
+
+        startGameAIxAI(board, WHITEai, BLACKai)
+    else:
+        opponentAI = AI(board, not playerSide, aiDepth)
+        startGameAi(board, playerSide, opponentAI)
+
 except KeyboardInterrupt:
     sys.exit()
